f79d11b861ff948d2b1a50c71674d8f7553f61ce
allanapavao <allanarodrigues.p@gmail.com>
2026-04-14 16:16:07 -0300

    style: delete comments

--- schemas/usuario.py
+++ schemas/usuario.py
@@ -11,42 +11,19 @@ class UsuarioSchema(BaseModel):
     nome_usuario: str = "Fulano de Souza Rodrigues"
     senha: str = "senha123"
 
+
 class UsuarioBuscaSchema(BaseModel):
     """ Define como deve ser a estrutura que representa a busca.
         A busca será feita apenas com base no uuid do usuario.
     """
     id_usuario: str = "7a743fa4-57b5-4b0b-b97a-5da34a58bf62"
 
+
 class ListagemUsuariosSchema(BaseModel):
     """ Define como uma listagem de produtos será retornada.
     """
     usuarios: List[UsuarioSchema]
 
-# def apresenta_usuario(usuario:Usuario):
-#     """ Retorna uma representação do usuario seguindo o schema definido em
-#         UsuarioViewSchema.
-#     """
-#     return {
-#             "usuario_id": usuario.usuario_id,
-#             "email": usuario.email,
-#             "nome_usuario": usuario.nome_usuario,
-#             "data_criacao": usuario.data_criacao
-#         }
-
-# def apresenta_usuarios(usuarios:List[Usuario]):
-#     """ Retorna uma representação do usuario seguindo o schema definido em
-#         UsuarioViewSchema.
-#     """
-#     result = []
-#     for usuario in usuarios:
-#         result.append({
-#             "usuario_id": usuario.usuario_id,
-#             "email": usuario.email,
-#             "nome_usuario": usuario.nome_usuario,
-#             "data_criacao": usuario.data_criacao
-#         })
-
-#     return {"usuarios": result}
 
 class UsuarioViewSchema(BaseModel):
     """Define como o usuario será retornado
