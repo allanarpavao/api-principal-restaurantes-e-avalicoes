73867d0916298c1cdf7dde3aab802ffc8f6cc560
allanapavao <allanarodrigues.p@gmail.com>
2026-05-26 09:29:16 -0300

    chore: change requirements, add conftest

--- tests/routes/test_usuario.py
+++ tests/routes/test_usuario.py
@@ -1,58 +1,15 @@
 import uuid
 import logging
-import pytest
 from pydantic import ValidationError
-from datetime import datetime
-from types import SimpleNamespace
+
 from http import HTTPStatus
-from unittest.mock import patch
 from sqlalchemy.exc import IntegrityError
 
-from app import app as flask_app
 from schemas.usuario import UsuarioViewSchema
 
 ##  python -m pytest -rA tests/routes/test_usuario.py
 
 logger = logging.getLogger(__name__)
-
-
-@pytest.fixture
-def client():
-    flask_app.config["TESTING"] = True
-    with flask_app.test_client() as client:
-        yield client
-
-@pytest.fixture
-def mock_db_session():
-    def fake_automatic_data_db(user):
-        user.usuario_id = str(uuid.uuid4())
-        user.data_criacao = datetime(2026, 4, 14, 0, 0, 0)
-
-        return user
-    
-    with patch("routes.usuario.Session") as mock_session:
-        mock_session.add.side_effect = fake_automatic_data_db
-        yield mock_session
-
-
-
-@pytest.fixture
-def mock_user_generator():
-    def _create_users(amount=1):
-        users = []
-        for i in range(amount):
-            users.append(
-                SimpleNamespace(
-                    usuario_id=str(uuid.uuid4()),
-                    nome_usuario=f"Test User {i}",
-                    email=f"test{i}@example.com",
-                    data_criacao=datetime(2026, 4, 14, 0, 0, 0)
-                )
-            )
-        return users
-    
-    return _create_users
-
 
 def test_create_valid_user_returns_201(mock_db_session, client):
     payload = {
